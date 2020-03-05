314886a9eaca84124042aa613c12aa2152810307
friendlyarm <support@friendlyarm.com>
2020-03-05 23:26:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,6 +60,6 @@ sudo apt-get -y install android-tools-fsutils
 sudo apt-get -y install bc lzop xfonts-utils xfonts-utils xfonts-utils xsltproc libjson-perl
 
 # for openwrt armhf
-sudo apt-get -y linux-libc-dev:i386
+sudo apt-get -y install linux-libc-dev:i386
 
 exit 0
